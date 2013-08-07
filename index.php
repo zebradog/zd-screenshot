043e9de8165da27b5bf2f7bafb2a6a55fc0893f2
Matt Cook <matt@zebradog.com>
2013-08-07 17:06:48 -0500

    Updated version?

--- index.php
+++ index.php
@@ -5,15 +5,19 @@ $width = isset($_GET['width']) ? $_GET['width'] : 1080;
 $url = isset($_GET['url']) ? $_GET['url'] : 'http://localhost/';
 $delay = isset($_GET['delay']) ? $_GET['delay'] : 3;
 $refresh = isset($_GET['refresh']) ? 1 : 0;
+$delete = isset($_GET['delete']) ? 1 : 0;
 $name = md5($url.$height.$width);
 $filename = $name.'-full.png';
 $directory = getcwd().'/img';
 $path = $directory.'/'.$filename;
+$file_exists = file_exists($path);
 
-if($refresh || !file_exists($path)){
-	exec('/usr/bin/python '.getcwd().'/webkit2png.py -W '.$width.' -H '.$height.' -o '.$name.' -F -D '.$directory.' --delay='.$delay.' '.$url.' --transparent');
+if($delete){
+	if($file_exists) unlink($path);
+}else if($refresh || !$file_exists){
+	exec('/usr/bin/python '.getcwd().'/webkit2png.py -W '.$width.' -H '.$height.' -o '.$name.' -F -D '.$directory.' --delay='.$delay.' '.$url.' --transparent');	
 }
-
-header('Content-type:image/png');
-
-readfile($path);
+if(!$delete){
+	header('Content-type:image/png');
+	readfile($path);
+}
